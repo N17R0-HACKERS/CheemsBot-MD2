6e888bd2a697a81391413964398225ca627688ef
ᴡʜᴏ ɪ'ᴀᴍ ..? <72617624+N17R0-HACKERS@users.noreply.github.com>
2022-06-15 03:04:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['916909137213']
-global.premium = ['916909137213']
-global.ownernomer = '916909137213'
-global.ownername = '🦄Dream Guy Xeon'
-global.botname = 'Cheems Bot MD V2'
-global.footer = 'Xeon Bot Inc.'
-global.ig = 'https://github.com/DGXeon'
+global.owner = ['918591220868']
+global.premium = ['918928986621']
+global.ownernomer = '918928986621'
+global.ownername = 'Rehan'
+global.botname = 'GOD-BOT'
+global.footer = 'Rehan Bot Inc.'
+global.ig = 'https://github.com/N17R0-HACKERS'
 global.region = 'India, NorthEast, Mizoram'
-global.sc = 'https://github.com/DGXeon/CheemsBot-MD2'
-global.myweb = 'https://youtu.be/imFIX-Wrt3s'
-global.packname = 'Cheems Bot'
-global.author = '🦄Dream Guy Xeon'
+global.sc = 'https://github.com/N17R0-HACKERS/CheemsBot-MD2'
+global.myweb = 'https://instagram.com/itsmerehan__'
+global.packname = 'God-Bot'
+global.author = 'Rehan'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
